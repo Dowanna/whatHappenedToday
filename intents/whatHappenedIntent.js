e1d36752bbfe585ec00a99417a31079b476973cc
shunya matsubara <dowanna@shunyanoMacBook-Pro.local>
2018-08-13 00:09:38 +0900

    separate functions

--- intents/whatHappenedIntent.js
+++ intents/whatHappenedIntent.js
@@ -6,63 +6,100 @@ const NEXT_FACT = "nextFact";
 const USER_DATE = "userDate";
 const FACT_CACHE = "factCache";
 
-// Today
-const today = new Date();
-const dd = today.getDate().toString();
-const mm = (today.getMonth() + 1).toString();
-
 // Database
 const factDynamoTableName = "alexa-fact-table";
 
-// Helper Method
+// Helper Method to change 811 -> 0811
 function pad(n, width, z) {
   z = z || "0";
   n = n + "";
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-let WhatHappenedIntent = function() {
-  let dynamoDb = new AWS.DynamoDB.DocumentClient();
-  let userDate = this.attributes[USER_DATE];
-  let message = "";
-
+function ifNullSetToday(userDate) {
+  const today = new Date();
   if (!userDate) {
-    userDate = pad(mm, 2).concat("月", pad(dd, 2), "日");
+    userDate = pad((today.getMonth() + 1).toString(), 2).concat(
+      "月",
+      pad(today.getDate().toString(), 2),
+      "日"
+    );
     console.log(`using default date, today ${userDate}`);
+    return userDate;
+  } else {
+    return userDate;
   }
+}
 
-  // user wants to hear about diff date
-  if (this.event.request.intent.slots != null) {
-    if (this.event.request.intent.slots.UserDate.value != null) {
-      let rawUserDates = this.event.request.intent.slots.UserDate.value.split(
-        "-"
-      );
+function ifExistsSetUserInput(slots, userDate) {
+  if (slots != null) {
+    if (slots.UserDate.value != null) {
+      let rawUserDates = slots.UserDate.value.split("-");
       userDate = "".concat(rawUserDates[1], "月", rawUserDates[2], "日");
       console.log(`Got user date ${userDate}`);
     }
-    this.attributes[USER_DATE] = userDate;
   }
+  return userDate;
+}
 
+function generateMessage(prefix, data, factNumber, userDate) {
+  return prefix.concat(
+    `${data.facts[factNumber].year}の${userDate}, ${
+      data.facts[factNumber].fact
+    }。`
+  );
+}
+
+let WhatHappenedIntent = function() {
+  let dynamoDb = new AWS.DynamoDB.DocumentClient();
+  let message = "";
+
+  // if first session, no USER_DATE specified in session, so use today as default date
+  userDate = ifNullSetToday(this.attributes[USER_DATE]);
+
+  // if user wants to hear about diff date
+  userDate = ifExistsSetUserInput(this.event.request.intent.slots, userDate);
+
+  this.attributes[USER_DATE] = userDate;
+
+  // initialize cache object and nextFactNumber if nonexisting
   if (!this.attributes[FACT_CACHE]) {
+    console.log("first ever session. Initialize FACT_CACHE attribute");
     this.attributes[FACT_CACHE] = {};
   }
+  if (!this.attributes[NEXT_FACT]) {
+    console.log("first ever session. Initialize NEXT_FACT attribute");
+    this.attributes[NEXT_FACT] = {};
+  }
 
-  if (this.attributes[FACT_CACHE][userDate]) {
-    console.log(`responding from cache`);
-    let data = this.attributes[FACT_CACHE][userDate];
-    message = message.concat(
-      `${data.facts[this.attributes[NEXT_FACT][userDate]].year}の${userDate}, ${
-        data.facts[this.attributes[NEXT_FACT][userDate]].fact
-      }。`
+  let todaysFactNumber = this.attributes[NEXT_FACT][userDate];
+  let todaysFactCache = this.attributes[FACT_CACHE][userDate];
+
+  if (todaysFactCache) {
+    if (!todaysFactNumber) {
+      this.attributes[NEXT_FACT][userDate] = todaysFactCache.facts.length - 1;
+    }
+
+    if (todaysFactNumber < 0) {
+      console.log("no more data");
+      message = message.concat("もうデータが無いので、最初から繰り返すね。");
+      this.attributes[NEXT_FACT][userDate] = todaysFactCache.facts.length - 1;
+    }
+
+    message = generateMessage(
+      message,
+      todaysFactCache,
+      this.attributes[NEXT_FACT][userDate],
+      userDate
     );
 
-    // Update session data
     this.attributes[NEXT_FACT][userDate] -= 1;
 
     this.emit(":ask", message, dialogues.REPROMPT);
     return;
   }
 
+  // if no cache, fetch from db
   const param = {
     TableName: factDynamoTableName,
     KeyConditionExpression: "#date=:date",
@@ -82,31 +119,20 @@ let WhatHappenedIntent = function() {
 
     let data = result.Items[0];
 
+    // store as cache
     this.attributes[FACT_CACHE][userDate] = data;
 
-    if (!this.attributes[NEXT_FACT]) {
-      console.log(`NEXT_FACT session attribute does not exist`);
-      this.attributes[NEXT_FACT] = {};
-    }
-
-    if (!this.attributes[NEXT_FACT][userDate]) {
-      console.log(`NEXT_FACT session attribute for today does not exist`);
+    if (!todaysFactNumber) {
       this.attributes[NEXT_FACT][userDate] = data.facts.length - 1;
     }
 
-    if (this.attributes[NEXT_FACT][userDate] < 0) {
-      console.log("もうデータがないよ");
-      message = message.concat("もうデータが無いので、最初から繰り返すね。");
-      this.attributes[NEXT_FACT][userDate] = data.facts.length - 1;
-    }
-
-    message = message.concat(
-      `${data.facts[this.attributes[NEXT_FACT][userDate]].year}の${userDate}, ${
-        data.facts[this.attributes[NEXT_FACT][userDate]].fact
-      }。`
+    message = generateMessage(
+      message,
+      data,
+      this.attributes[NEXT_FACT][userDate],
+      userDate
     );
 
-    // Update session data
     this.attributes[NEXT_FACT][userDate] -= 1;
 
     this.emit(":ask", message, dialogues.REPROMPT);
